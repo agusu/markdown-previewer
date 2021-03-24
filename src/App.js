ca568a561388ade898dd24e6cf4fadaae5939a55
Agustin Del Torto <aguss15dt@gmail.com>
2021-03-24 15:23:14 -0300

    pruebita 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Prueba prueba
+          Prueba prueba 2
         </p>
         <a
           className="App-link"
